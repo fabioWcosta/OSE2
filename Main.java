8b7052e7956a697db53c6698c106030cd4cf15ae
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-07-17 23:58:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
         int dicesCha = (int) (1 + random * (18-3));
 
         
-        System.out.println("Habilidades do Personagem:")
+        System.out.println("Habilidades do Personagem:");
         System.out.println("For: " + dicesStr);
         System.out.println("Des: " + dicesDex);
         System.out.println("Con: " + dicesCon);
