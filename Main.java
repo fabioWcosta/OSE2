d8339d4b89b960ec1e28a25920f8376da0fa9138
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-09-10 00:01:51 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -61,7 +61,7 @@ public class Main {
             double random = Math.random();
             int moralResult = (int) ((int) 1+(random *(12-2)));
 
-            System.out.println("Resultado do teste de Moral:" + );
+            System.out.println("Resultado do teste de Moral:" + moralResult);
 
             if(moralResult <= 6){
                 System.out.println("Falha no teste!");
