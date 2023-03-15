1e999cc708b8cccd8739d35e28628935a70d2749
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-03-14 22:49:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -40,6 +40,7 @@ public class Main {
         double randomNumber = Math.random();
         int supresa = (int) (1 + randomNumber * (18));
         System.out.println("Resultado do encontro:" + supresa);
+        if
 
         //VERIFICAÇÃO DE MORAL 
         
