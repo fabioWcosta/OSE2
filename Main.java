436371bf37ad5b19fae2fcb48a582501e7a4290c
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-03-16 19:39:13 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,10 +23,10 @@ public class Main {
         double moralRandom = Math.random();
 
         int dicesStr = (int) (3 + strRandom * (15));
-        int dicesDex = (int) (3 + dexRandom * (15));
-        int dicesCon = (int) (3 + conRandom * (15));
         int dicesInt = (int) (3 + intRandom * (15));
         int dicesWis = (int) (3 + wisRandom * (15));
+        int dicesDex = (int) (3 + dexRandom * (15));
+        int dicesCon = (int) (3 + conRandom * (15));
         int dicesCha = (int) (3 + chaRandom * (15));
 
         System.out.println("Atributos :");
