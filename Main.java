5399324c1628d4832793b883dba6bfe212df03fb
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-03-15 23:55:14 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -44,7 +44,7 @@ public class Main {
         if(surpresa >= 4){
             System.out.println("Os monstros são supreendidos!!!");
         }else{
-            
+            System.out.println();
         }
 
         //VERIFICAÇÃO DE MORAL 
