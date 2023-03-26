e3af6ccd91911e6c0679175cad30b0892b08778b
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-03-25 23:56:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import javax.sound.midi.Soundbank;
 public class Main {
     public static void main(String[] args){
 
-        String oponenteA;
+        String nomeOponenteA;
         String oponenteB;
 
         int Str;
