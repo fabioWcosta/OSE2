d44fd6453510a4422474b68cd343fcc78a12ce2a
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-03-29 23:48:16 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args){
 
         public String nomeOponenteA;
-        String nomeOponenteB;
+        public String nomeOponenteB;
 
-        int Str;
+        public int Str;
         int Int;
         int Wis;
         int Dex;
