ac89c98a88dc64ed1e39b6271415d15367e584f8
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-04-08 22:40:09 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args){
 
-        //VARIAVEIS
+        //VARIÁVEIS
         public String nomeOponenteA;
         public String nomeOponenteB;
 
