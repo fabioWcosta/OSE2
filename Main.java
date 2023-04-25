0997e54f1854a86b31215b3b3a7cbc66fd138ff8
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-04-24 23:49:51 -0300

    Class Opponent

--- Main.java
+++ Main.java
@@ -9,20 +9,6 @@ public class Main {
         System.out.println("Os dois oponentes se encontram...");
         System.out.println(" que comece o combate.");
 
-        double strRandom = Math.random();
-        double intRandom = Math.random();
-        double wisRandom = Math.random();
-        double dexRandom = Math.random();
-        double conRandom = Math.random();
-        double chaRandom = Math.random();
-        double moralRandom = Math.random();
-
-        int dicesStr = (int) (3 + strRandom * (15));
-        int dicesInt = (int) (3 + intRandom * (15));
-        int dicesWis = (int) (3 + wisRandom * (15));
-        int dicesDex = (int) (3 + dexRandom * (15));
-        int dicesCon = (int) (3 + conRandom * (15));
-        int dicesCha = (int) (3 + chaRandom * (15));
 
         System.out.println("Atributos :");
         System.out.println("For: " + dicesStr);
--- Opponent.java
+++ Opponent.java
@@ -1,4 +1,10 @@
 
 public class Opponent {
-
+    double strRandom = Math.random();
+    double intRandom = Math.random();
+    double wisRandom = Math.random();
+    double dexRandom = Math.random();
+    double conRandom = Math.random();
+    double chaRandom = Math.random();
+    double moralRandom = Math.random();
 }
