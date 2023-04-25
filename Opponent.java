f70cd6cf7dbb4bd9369acf872c2bad88eafb7327
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-04-24 23:50:23 -0300

    Update Opponent.java

--- Opponent.java
+++ Opponent.java
@@ -7,4 +7,11 @@ public class Opponent {
     double conRandom = Math.random();
     double chaRandom = Math.random();
     double moralRandom = Math.random();
+
+    int dicesStr = (int) (3 + strRandom * (15));
+    int dicesInt = (int) (3 + intRandom * (15));
+    int dicesWis = (int) (3 + wisRandom * (15));
+    int dicesDex = (int) (3 + dexRandom * (15));
+    int dicesCon = (int) (3 + conRandom * (15));
+    int dicesCha = (int) (3 + chaRandom * (15));
 }
