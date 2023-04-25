10a42dfad5994e7ea5b6b2a35d168e4893a6434e
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-04-24 23:57:56 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,6 +9,30 @@ public class Main {
         System.out.println("Os dois oponentes se encontram...");
         System.out.println(" que comece o combate.");
 
+        double strRandom = Math.random();
+        double intRandom = Math.random();
+        double wisRandom = Math.random();
+        double dexRandom = Math.random();
+        double conRandom = Math.random();
+        double chaRandom = Math.random();
+        double moralRandom = Math.random();
+
+        int dicesStr = (int) (3 + strRandom * (15));
+        int dicesInt = (int) (3 + intRandom * (15));
+        int dicesWis = (int) (3 + wisRandom * (15));
+        int dicesDex = (int) (3 + dexRandom * (15));
+        int dicesCon = (int) (3 + conRandom * (15));
+        int dicesCha = (int) (3 + chaRandom * (15));
+
+        System.out.println("Atributos :");
+        System.out.println("For: " + dicesStr);
+        System.out.println("Int: " + dicesInt);
+        System.out.println("Wis: " + dicesWis);
+        System.out.println("Des: " + dicesDex);
+        System.out.println("Con: " + dicesCon);
+        System.out.println("Car: " + dicesCha);
+        System.out.println();
+
         //VERIFICAÇÃO DE SURPRESA!
         double randomNumber = Math.random();
         int surpresa = (int) (1 + randomNumber * (18));
