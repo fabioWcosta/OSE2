704521efef03c8f9a65c71f73786c4ce376c648e
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-09-21 23:48:05 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -66,7 +66,7 @@ public class Main {
         String nome3 = new String("Gustav");
         String res; 
         res = (nome1==nome2)? "igual":"São diferentes";
-        res = (nome1.equals(nome2))? "É igual!":"Não, é diferente";
+        res = (nome1.equals(nome3))? "É igual!":"Não, é diferente";
         System.out.println(res);
 
     }
