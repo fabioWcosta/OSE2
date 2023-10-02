774e57ebc73916a5e0b556ba2fdbcbc5e36de0c8
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-10-01 23:59:09 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -70,7 +70,7 @@ public class Main {
         System.out.println(res);
 
         System.out.println("onde vc vai, Raul?");
-        System.out.println("isso!");
+        System.out.println(isso!);
 
     }
     //Ataque de um personagem a outro:
