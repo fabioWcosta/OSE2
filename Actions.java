d327fd162ada06783b6722525dd4d1a1649124be
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2023-10-16 23:58:12 -0300

    Update Actions.java

--- Actions.java
+++ Actions.java
@@ -25,14 +25,5 @@ public class Actions {
         }
 
         
-
-        //Os oponentes comparam suas iniciativas (em 1D6):
-        public int initiative(){
-            System.out.println();
-            System.out.println("Os oponentes se movem e o combate começa!");
-            double randIni = Math.random();
-            int iniResult = (int) ((int) 1 + randIni * (6 - 1));
-            System.out.println(iniResult);
-            return iniResult;
         }
 }
